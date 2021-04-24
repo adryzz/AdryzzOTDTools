0406b7609664a1cc0a54f243bfaf17c1fd50d950
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-24 19:04:28 +0200

    update to new OpenTabletDriver.Plugin version

--- AudioToggle/AudioToggleBinding.cs
+++ AudioToggle/AudioToggleBinding.cs
@@ -1,111 +1,146 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using AudioToggle.Platforms.Linux;
+using AudioToggle.Platforms.Windows;
 using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
 
 namespace AudioToggle
 {
     [PluginName("Audio Toggle"), SupportedPlatform(PluginPlatform.Windows /*| PluginPlatform.Linux*/)]
-    public class AudioToggleBinding : IValidateBinding, IBinding
+    public class AudioToggleBinding : IBinding
     {
+        public static string[] ValidProperties => new string[] { "Toggle output device 1", "PTT output device 1", "Toggle input device 1", "PTT input device 1", "Toggle output device 2", "PTT output device 2", "Toggle input device 2", "PTT input device 2", "List audio devices" };
 
-        [Property("Property")]
-        public string Property { get; set; }
+        [Property("Action"), PropertyValidated(nameof(ValidProperties))]
+        public string Property { set; get; }
 
-        public Action Press => (Action)RunAction;
+        public static IAudioToggle Instance;
 
-        public Action Release => (Action)StopAction;
+        public static int[] OutputDevices = new int[2] { -1, -1 };
+        public static int[] InputDevices = new int[2] { -1, -1 };
 
-        public string[] ValidProperties => new string[] { "Toggle output device 1", "PTT output device 1", "Toggle input device 1", "PTT input device 1", "Toggle output device 2", "PTT output device 2", "Toggle input device 2", "PTT input device 2", "List audio devices" };
 
-        void RunAction()
+        [Property("Output device 1 index"), DefaultPropertyValue(-1)]
+        public int OutputDevice1 { get => OutputDevices[0]; set => OutputDevices[0] = value; }
+
+        [Property("Input device 1 index"), DefaultPropertyValue(-1)]
+        public int InputDevice1 { get => InputDevices[0]; set => InputDevices[0] = value; }
+
+        [Property("Output device 2 index"), DefaultPropertyValue(-1)]
+        public int OutputDevice2 { get => OutputDevices[1]; set => OutputDevices[1] = value; }
+
+        [Property("Input device 1 index"), DefaultPropertyValue(-1)]
+        public int InputDevice2 { get => InputDevices[1]; set => InputDevices[1] = value; }
+
+        void IBinding.Press(IDeviceReport report)
         {
-            if (AudioToggleTool.Instance == null || AudioToggleTool.Instance.Disposed)
+            if (Instance == null || Instance.Disposed)
             {
-                Log.Write("AudioToggle", "Tool instance not initialized", LogLevel.Error);
-                return;
+                Initialize();
             }
 
             switch (Property)
             {
                 case "Toggle output device 1":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.OutputDevices[0]);
+                        Instance.ToggleOutputDevice(OutputDevices[0]);
                         break;
                     }
                 case "PTT output device 1":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.InputDevices[0]);
+                        Instance.ToggleOutputDevice(InputDevices[0]);
                         break;
                     }
                 case "Toggle input device 1":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[0]);
+                        Instance.ToggleInputDevice(InputDevices[0]);
                         break;
                     }
                 case "PTT input device 1":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[0]);
+                        Instance.ToggleInputDevice(InputDevices[0]);
                         break;
                     }
                 case "Toggle output device 2":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.OutputDevices[1]);
+                        Instance.ToggleOutputDevice(OutputDevices[1]);
                         break;
                     }
                 case "PTT output device 2":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.InputDevices[1]);
+                        Instance.ToggleOutputDevice(InputDevices[1]);
                         break;
                     }
                 case "Toggle input device 2":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[1]);
+                        Instance.ToggleInputDevice(InputDevices[1]);
                         break;
                     }
                 case "PTT input device 2":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[1]);
+                        Instance.ToggleInputDevice(InputDevices[1]);
                         break;
                     }
                 case "List audio devices":
                     {
-                        AudioToggleTool.Instance.ListDevices();
+                        Instance.ListDevices();
                         break;
                     }
             }
         }
-        void StopAction()
+
+        void IBinding.Release(IDeviceReport report)
         {
-            if (AudioToggleTool.Instance == null || AudioToggleTool.Instance.Disposed)
+            if (Instance == null || Instance.Disposed)
             {
-                Log.Write("AudioToggle", "Tool instance not initialized", LogLevel.Error);
-                return;
+                Initialize();
             }
 
             switch (Property)
             {
                 case "PTT output device 1":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.InputDevices[0]);
+                        Instance.ToggleOutputDevice(InputDevices[0]);
                         break;
                     }
                 case "PTT input device 1":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[0]);
+                        Instance.ToggleInputDevice(InputDevices[0]);
                         break;
                     }
                 case "PTT output device 2":
                     {
-                        AudioToggleTool.Instance.ToggleOutputDevice(AudioToggleTool.InputDevices[1]);
+                        Instance.ToggleOutputDevice(InputDevices[1]);
                         break;
                     }
                 case "PTT input device 2":
                     {
-                        AudioToggleTool.Instance.ToggleInputDevice(AudioToggleTool.InputDevices[1]);
+                        Instance.ToggleInputDevice(InputDevices[1]);
                         break;
                     }
+            }
+        }
+
+        void Initialize()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (Instance == null || Instance.Disposed)
+                {
+                    Instance = new WindowsAudioToggle();
+                    Instance.Initialize();
+                }
+            }
+            else
+            {
+                if (Instance == null || Instance.Disposed)
+                {
+                    Instance = new LinuxAudioToggle();
+                    Instance.Initialize();
+                }
             }
         }
     }
